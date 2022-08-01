b4e62a19f1c52f1eca945f9793493284e3ce0119
Luis Martins <chateadorn@gmail.com>
2022-08-01 16:34:47 -0300

    alterando nome da variável + testando payload + movimentação

--- Car/Car.ino
+++ Car/Car.ino
@@ -3,7 +3,7 @@
 
 char i;
 
-int v = 150;
+int speed = 150;
 
 int frontlight = 23;
 int backlight = 22;
@@ -23,10 +23,60 @@ void setup() {
   Serial3.begin(3);
 }
 
+void loop() {
+  connect();
+  payload();
+}
+
 void connect() {
   if (Serial3.available()) {
     i = Serial3.read();
 
     Serial.println(i);
   }
+}
+
+void payload() {
+  if (i == 'F') {
+    
+    follow();
+    
+  } else if (i == 'B') {
+    
+    back(); 
+  }
+}
+
+// Definindo a movimentação do robô \\
+
+void follow() {
+  left_engine.run(FORWARD);
+  left_engine.setSpeed(speed);
+
+  right_engine.run(FORWARD);
+  right_engine.setSpeed(speed);
+}
+
+void back() {
+  left_engine.run(BACKWARD);
+  left_engine.setSpeed(speed);
+
+  right_engine.run(BACKWARD);
+  right_engine.setSpeed(speed);
+}
+
+void right() {
+  
+}
+
+void left() {
+  
+}
+
+void fRight() {
+  
+}
+
+void fLeft() {
+  
 }
